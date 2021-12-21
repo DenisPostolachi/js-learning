8f19376a55a72f466f890449221755fdd8c13fd2
Deny <deny.postolachi88@gmail.com>
2021-12-22 00:28:57 +0200

    exc add

--- Objects/Задача №6 - массив объектов/app.js
+++ Objects/Задача №6 - массив объектов/app.js
@@ -1,7 +1,11 @@
+let points = [
+    [1, 2],
+    [3, 4],
+    [null,  4],
+    [3, undefined]
+]
+const checkPoints = (arr) => {
+    return arr.filter(item => item[0] && item[1])
+};
 
-
-
-
-
-
-
+console.log(checkPoints(points));
