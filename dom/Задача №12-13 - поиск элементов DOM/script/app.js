041c439d912e4b8d88e1e134a4887e0196ecf5a2
DenyPostolachi <89935578+DenyPostolachi@users.noreply.github.com>
2021-12-24 10:53:32 +0200

    Delete dom directory

--- dom/Задача №12-13 - поиск элементов DOM/script/app.js
+++
@@ -1,5 +0,0 @@
-const elem = document.querySelector('#elem');
-let firstChild = elem.firstElementChild;
-firstChild.style.color = 'red';
-
-elem.previousElementSibling.textContent += ' !!!!!!';
--- dom/Задача №14 - поиск элементов DOM/script/app.js
+++
@@ -1,7 +0,0 @@
-const parent = document.querySelector('#parent');
-const child = document.querySelector('#child');
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-    parent.removeChild(child);
-});
