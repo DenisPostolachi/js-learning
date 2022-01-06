5e593a308dd3f972fd383a2a04d7e04cada25c9a
deny <deny.postolachi88@gmail.com>
2022-01-06 16:57:08 +0200

    exc 8 refactor

--- dom/Задача №8 - добавление элементов в DOM/script/app.js
+++ dom/Задача №8 - добавление элементов в DOM/script/app.js
@@ -1,36 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('input');
-    const addButton = document.getElementById('addToList');
-    const ol = document.getElementById('orderedList');
-    input.focus()
-    //todo .disabled
-    addButton.setAttribute('disabled', 'true');
+let button = document.querySelector(".btn");
+let input = document.querySelector(".input");
+let form = document.querySelector(".form");
+let list = document.querySelector(".list li");
 
-    //todo use addEventListener
-    input.onkeyup = () => {
-        //todo if (input.value.length)
-        if (input.value.length > 0) {
-            addButton.removeAttribute('disabled');
-        }
+button.disabled = true;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = form.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const input = document.createElement('span');
+    input.textContent = value;
+    li.appendChild(input);
+    list.appendChild(li);
+    form.reset();
+    button.disabled = true;
+});
+
+
+input.addEventListener('input', (e) => {
+
+    if (!e.target.value) {
+        button.disabled = true
+    } else {
+        button.disabled = false
     }
 
-    //todo use addEventListener
-    addButton.onclick = () => {
-        const li = document.createElement("li");
-        li.classList.add('li');
-        // todo всегда в доме должны быть 2 тэга рядом, .append
-        li.appendChild(document.createTextNode(`${input.value}`));
-        ol.appendChild(li);
-        //todo ""?
-        input.value = null;
-        addButton.setAttribute('disabled', 'true');
-    }
-
-    //todo use addEventListener
-    //todo ставить листенер на список
-    window.onclick = e => {
-        if (e.target.tagName === 'LI') {
-            alert(e.target.innerText);
-        }
-    }
 })
+
+list.addEventListener('click', (e)  => {
+    alert(e.target.innerText);
+});
+
+
+
+
+
