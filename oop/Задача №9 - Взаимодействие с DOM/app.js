2b7badc0a590c7e0040e81f77b4845e8c0d2ced8
deny <deny.postolachi88@gmail.com>
2022-01-15 16:35:43 +0200

    refactor exc.add

--- oop/Задача №9 - Взаимодействие с DOM/app.js
+++ oop/Задача №9 - Взаимодействие с DOM/app.js
@@ -7,7 +7,11 @@ form.style.flexDirection = 'column';
 container.append(form)
 
 class Form {
-    constructor(type, id, placeholder, value, rules = {required: true, min: true}) {
+    constructor(type, id, placeholder, value, rules = {
+                    required: value,
+                    min: v => v < 10
+                }
+    ) {
         this.type = type;
         this.id = id;
         this.placeholder = placeholder;
@@ -27,16 +31,21 @@ class Form {
         this.input.setAttribute('id', `${this.id}`);
         this.input.setAttribute('placeholder', `${this.placeholder}`);
         this.input.setAttribute('value', `${this.value}`);
-        this.input.setAttribute('rules', `${this.rules}`);
+        this.input.setAttribute('rules', `${[]}`);
         return this
     }
 
+    setValidationRule(ruleValue) {
+        console.log(this.rules.required)
+    }
 
 
     validate() {
         form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            console.log(this.rules)
+            if (this.rules.required && !this.value) {
+                return e.preventDefault();
+            }
+
         })
         return this
     }
@@ -45,12 +54,12 @@ class Form {
 }
 
 
-const formElementOne = new Form('text', 'name', 'Name', 'John', );
+const formElementOne = new Form('text', 'name', 'Name', 'John', [rules.required]);
 const formElementTwo = new Form('text', 'email', 'Email', 'mail@mail.com',);
-const formElementThree = new Form('text', 'age', 'Age', '62', );
+const formElementThree = new Form('text', 'age', 'Age', '62',);
 const formElementFour = new Form('text', 'birthdate', 'Date', '01.12.1962',);
 const formElementFive = new Form('submit', 'submit', '', 'Submit');
-formElementOne.create()
+formElementOne.create().setValidationRule()
 formElementTwo.create()
 formElementThree.create()
 formElementFour.create()
