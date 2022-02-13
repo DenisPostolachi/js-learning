d6b942a609e03fe6605bd61ed2b21dfc7550dfee
Deny <denis.postolachi88@gmail.com>
2022-02-13 20:43:18 +0200

    init

--- Задача №1 - Работа с интервалами/app.js
+++ Задача №1 - Работа с интервалами/app.js
@@ -4,28 +4,24 @@ const dataContainer = document.getElementById('data-container');
 
 
 btn.addEventListener('click', () => {
-    let currentDate = new Date();
-    let pastDate = new Date(input.value);
+    const currentDate = new Date();
+    const pastDate = new Date(input.value);
 
-    const calcDate = (date1,date2) => {
+    const calcDate = (date1, date2) => {
         let difference = Math.floor(date1.getTime() - date2.getTime());
         let day = 1000 * 60 * 60 * 24;
 
-        let days = Math.floor(difference/day);
-        let months = Math.floor(days/31);
-        let years = Math.floor(months/12);
+        const days = Math.floor(difference / day);
+        const months = Math.floor(days / 31);
+        const years = Math.floor(months / 12);
 
-        let yearElement = document.createElement('p');
+        const yearElement = document.createElement('p');
         yearElement.innerText = `${years} years`
-        dataContainer.append(yearElement);
-
-        let monthElement = document.createElement('p');
+        const monthElement = document.createElement('p');
         monthElement.innerText = `${months} months`
-        dataContainer.append(monthElement);
-
-        let dayElement = document.createElement('p');
+        const dayElement = document.createElement('p');
         dayElement.innerText = `${days} days`
-        dataContainer.append(dayElement);
+        dataContainer.append(yearElement, monthElement, dayElement);
     }
 
     calcDate(currentDate, pastDate);
