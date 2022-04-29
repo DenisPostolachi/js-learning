931650c4fe531b78b007126c95ba6d34a6df69d0
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-04-29 17:25:57 +0300

    algorithms changes

--- Task #6/app.js
+++ Task #6/app.js
@@ -18,14 +18,16 @@ let data = {
 
 
 function createTree(container, data) {
-    const ul = container.appendChild(document.createElement('ul'));
+    const ul = document.createElement('ul');
     for (const [key, val] of Object.entries(data)) {
         const li = ul.appendChild(document.createElement('li'));
         li.innerText = key;
+
         if (Object.keys(val).length) {
             createTree(li, val);
         }
     }
+    container.appendChild(ul);
 }
 
 createTree(document.getElementById('container'), data);
