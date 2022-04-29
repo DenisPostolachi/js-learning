0ff38670503c03c239e3fdeb1b05b5d105b4d0a9
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-04-29 14:11:48 +0300

    loops, reformat

--- Task #2 - loops/app.js
+++ Task #2 - loops/app.js
@@ -1,4 +1,11 @@
 const numbersToCount = [1, 2, 4, 5];
 
-let evenSum = numbersToCount.reduce((acc, curr) => acc + (curr % 2 === 0 ? curr : 0), 0);
-console.log('Even sum:', evenSum);
+let evenSum = numbersToCount.reduce((sum, number) => {
+  if (number % 2 === 0) {
+    sum += number;
+  }
+
+  return sum;
+}, 0);
+
+console.log("Even sum:", evenSum);
