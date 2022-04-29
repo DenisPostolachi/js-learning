2b7a2285bbd63b55048718d3a261f1ccc578002c
Deny <deny.postolachi88@gmail.com>
2022-04-29 12:40:21 +0300

    content add

---
+++ Task #2 - array processing/app.js
@@ -0,0 +1,16 @@
+const numberArray = Array.from({length: 10}, () => Math.floor(Math.random() * 30));
+
+console.log(numberArray)
+
+console.log(numberArray.filter(item => item < 5))
+
+const max = Math.max(...numberArray);
+const min = Math.min(...numberArray);
+const indexMax = numberArray.indexOf(max);
+const indexMin = numberArray.indexOf(min);
+console.log(`index of min is ${indexMin} | index of max is ${indexMax}`)
+
+console.log(numberArray.sort((a, b) => b - a));
+
+const biggerThenAvg = numberArray.filter(value => value > numberArray.reduce((acc, currentValue) => acc + currentValue) / numberArray.length);
+console.log(`bigger then avg. are - ${biggerThenAvg}`)
