e6d18be4c36f844934ad4b1f625fa51fb1b73da5
deny <denis.postolachi88@gmail.com>
2022-05-09 22:50:21 +0300

    tudor`s correcting

--- Task #9 - date and time/app.js
+++ Task #9 - date and time/app.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById('btn');
-const textContainer = document.getElementById('text-container');
 const hoursElement = document.getElementById('hrs')
 const minutesElement = document.getElementById('mns')
 const secondsElement = document.getElementById('scs')
+const secondsContainer = document.getElementById('show-seconds')
+const minutesContainer = document.getElementById('show-minutes')
+const hoursContainer = document.getElementById('show-hours')
 
 
 hoursElement.addEventListener('keyup', ({target}) => {
@@ -33,7 +35,6 @@ secondsElement.addEventListener('input', ({target}) => {
 let minutes = 0;
 let hours = 0;
 let seconds = 0;
-let start = 0;
 
 const countdownTimer = () => {
     const hoursElement = document.getElementById('hrs')
@@ -49,63 +50,27 @@ const countdownTimer = () => {
 
     const updateTimer = () => {
         console.log(timer)
-        secondsElement.value = timer;
 
         if (!timer) {
             clearInterval(timeInterval)
         }
 
         timer--;
+
+        const hours = Math.floor(timer / (60 * 60));
+        let divisorForMinutes = timer % (60 * 60);
+        const minutes = Math.floor(divisorForMinutes / 60);
+        let divisorForSeconds = divisorForMinutes % 60;
+        const seconds = Math.ceil(divisorForSeconds);
+
+        secondsContainer.innerText = `${seconds}`
+        minutesContainer.innerText = `${minutes}`
+        hoursContainer.innerText = `${hours}`
+
     }
 
     timeInterval =  setInterval(updateTimer, 1000)
     //todo make method for update time in interface
-}
-
-const countdownTimer2 = () => {
-    if (start === 0 && minutesElement && secondsElement) {
-
-        minutes = Number(minutesElement.value);
-        seconds = Number(document.getElementById('scs').value);
-        hours = Number(document.getElementById('hrs').value);
-
-        if (isNaN(minutes)) minutes = 0;
-        if (isNaN(seconds)) seconds = 0;
-
-        minutesElement.value = minutes;
-        document.getElementById('scs').value = seconds;
-        document.getElementById('hrs').value = hours;
-        start = 1;
-        document.getElementById('btn').setAttribute('disabled', 'disabled');
-    }
-
-    if (minutes === 0 && seconds === 0 && hours === 0) {
-        start = 0;
-        document.getElementById('btn').removeAttribute('disabled');
-        textContainer.innerText = 'Count is over'
-        return false;
-    }
-
-    seconds--;
-    if (seconds < 0) {
-        if (minutes > 0) {
-            seconds = 59;
-            minutes--;
-        } else {
-            seconds = 0;
-            minutes = 0;
-        }
-    }
-
-    if (minutes === 0 && seconds === 0) {
-        hours--;
-        minutes = 60;
-    }
-
-    document.getElementById('showhrs').innerHTML = hours;
-    document.getElementById('showmns').innerHTML = minutes;
-    document.getElementById('showscs').innerHTML = seconds;
-    setTimeout('countdownTimer()', 1000);
 }
 
 btn.addEventListener('click', () => {
