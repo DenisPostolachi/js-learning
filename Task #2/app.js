5082c03c485c4b766dd527f7612a1398aa378a19
deny <denis.postolachi88@gmail.com>
2022-05-10 13:34:39 +0300

    correct

--- Task #2/app.js
+++ Task #2/app.js
@@ -1,5 +1,10 @@
 const fib = n => {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    // return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    if (n <= 1) {
+        return n
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
 }
 
-console.log(fib(3))
+console.log(fib(7))
--- Task #3/app.js
+++ Task #3/app.js
@@ -1,6 +1,12 @@
 const factorial = n => {
     // todo переделать под if ... else
-    return (n !== 1) ? n * factorial(n - 1) : 1;
+
+    // return (n !== 1) ? n * factorial(n - 1) : 1;
+    if (n !== 1) {
+        return n * factorial(n - 1)
+    } else {
+        return 1
+    }
 }
 
 console.log(factorial(5))
