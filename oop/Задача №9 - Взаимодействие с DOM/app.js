e460d7789fe55233c822a59f0b2e042e401f36ef
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-05-11 16:52:18 +0300

    some changes

--- oop/Задача №9 - Взаимодействие с DOM/app.js
+++ oop/Задача №9 - Взаимодействие с DOM/app.js
@@ -25,7 +25,7 @@ class Form {
         this.input.setAttribute('id', `${this.id}`);
         this.input.setAttribute('placeholder', `${this.placeholder}`);
         this.input.setAttribute('value', `${this.value}`);
-        this.input.setAttribute('rules', `${Object.keys(this.rules)}`);
+        this.input.setAttribute('rules', this.rules);
         return this
     }
 
@@ -41,61 +41,95 @@ class Form {
     validate() {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
-            const name = document.getElementById('name');
-            const email = document.getElementById('email');
-            const age = document.getElementById('age');
-            const birthdate = document.getElementById('birthdate');
 
-            if ( name.hasAttribute('rules') && name.value) {
-                name.classList.remove('error');
-                name.classList.add('success');
-                document.getElementById('name-error').style.display = 'none'
-            } else {
-                name.classList.remove('success');
-                name.classList.add('error');
-                document.getElementById('name-error').style.display = 'block'
+            // todo find all inputs and validate they
+
+            const inputs = document.getElementsByTagName('input');
+
+            const errors = {}
+
+            for (const input of inputs) {
+                const rules = input.getAttribute('rules')
+
+                if (!rules) {
+                    return;
+                }
+
+                // todo make for all rules
+                // {
+                //      name: [ 'required' ],
+                //      email: ['invalid email']
+                // ]
+
+
+                if (rules.split('|').includes('required') && !input.value) {
+                    // todo if exists extents errors
+                    errors[input.id] = ['required field']
+                }
+
+
             }
 
-            if ( email.hasAttribute('rules') && email.value) {
-                email.classList.remove('error');
-                email.classList.add('success');
-                document.getElementById('email-error').style.display = 'none'
-            } else {
-                email.classList.remove('success');
-                email.classList.add('error');
-                document.getElementById('email-error').style.display = 'block'
-            }
+            return !!errors.length
 
-            if ( age.hasAttribute('rules') && Number(age.value) > 10) {
-                age.classList.remove('error');
-                age.classList.add('success');
-                document.getElementById('age-error').style.display = 'none'
-            } else {
-                age.classList.remove('success');
-                age.classList.add('error');
-                document.getElementById('age-error').style.display = 'block'
-            }
-
-            if ( birthdate.hasAttribute('rules') && birthdate.value) {
-                birthdate.classList.remove('error');
-                birthdate.classList.add('success');
-                document.getElementById('birthdate-error').style.display = 'none'
-            } else {
-                birthdate.classList.remove('success');
-                birthdate.classList.add('error');
-                document.getElementById('birthdate-error').style.display = 'block'
-            }
+            // const name = document.getElementById('name');
+            // const email = document.getElementById('email');
+            // const age = document.getElementById('age');
+            // const birthdate = document.getElementById('birthdate');
+            //
+            // if ( name.hasAttribute('rules') && name.value) {
+            //     name.classList.remove('error');
+            //     name.classList.add('success');
+            //     document.getElementById('name-error').style.display = 'none'
+            // } else {
+            //     name.classList.remove('success');
+            //     name.classList.add('error');
+            //     document.getElementById('name-error').style.display = 'block'
+            // }
+            //
+            // if ( email.hasAttribute('rules') && email.value) {
+            //     email.classList.remove('error');
+            //     email.classList.add('success');
+            //     document.getElementById('email-error').style.display = 'none'
+            // } else {
+            //     email.classList.remove('success');
+            //     email.classList.add('error');
+            //     document.getElementById('email-error').style.display = 'block'
+            // }
+            //
+            // if ( age.hasAttribute('rules') && Number(age.value) > 10) {
+            //     age.classList.remove('error');
+            //     age.classList.add('success');
+            //     document.getElementById('age-error').style.display = 'none'
+            // } else {
+            //     age.classList.remove('success');
+            //     age.classList.add('error');
+            //     document.getElementById('age-error').style.display = 'block'
+            // }
+            //
+            // if ( birthdate.hasAttribute('rules') && birthdate.value) {
+            //     birthdate.classList.remove('error');
+            //     birthdate.classList.add('success');
+            //     document.getElementById('birthdate-error').style.display = 'none'
+            // } else {
+            //     birthdate.classList.remove('success');
+            //     birthdate.classList.add('error');
+            //     document.getElementById('birthdate-error').style.display = 'block'
+            // }
 
         })
         return this
     }
+
+    //  todo add method get value
 }
 
-const formElementOne = new Form('text', 'name', 'Name', 'John', {required: true});
-const formElementTwo = new Form('text', 'email', 'Email', 'mail@mail.com', {required: true});
-const formElementThree = new Form('text', 'age', 'Age', '62', {min: true});
-const formElementFour = new Form('text', 'birthdate', 'Date', '01.12.1962', {required: true});
+const formElementOne = new Form('text', 'name', 'Name', 'John', 'required');
+const formElementTwo = new Form('text', 'email', 'Email', 'mail@mail.com', 'required|email');
+const formElementThree = new Form('text', 'age', 'Age', '62', 'required|min:10');
+const formElementFour = new Form('text', 'birthdate', 'Date', '01.12.1962', 'required|date');
 const formElementFive = new Form('submit', 'submit', '', 'Submit');
+
 formElementOne.create().createErrorMessage('name');
 formElementTwo.create().createErrorMessage('email');
 formElementThree.create().createErrorMessage('age');
